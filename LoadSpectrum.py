29562eddb3513cc9cb1c2cf855ebe28c053591e0
lerkoah <lerko.araya@ug.uchile.cl>
2020-07-21 15:17:55 -0400

    load spa function

---
+++ LoadSpectrum.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def read_spa(filepath):
+    with open(filepath, 'rb') as f:
+        f.seek(564)
+        Spectrum_Pts = np.fromfile(f, np.int32,1)
+        f.seek(30)
+        SpectraTitles = np.fromfile(f, np.uint8,255)
+        SpectraTitles = ''.join([chr(x) for x in SpectraTitles if x!=0])
+
+        f.seek(576)
+        Max_Wavenum=np.fromfile(f, np.single, 1)
+        Min_Wavenum=np.fromfile(f, np.single, 1)
+        Wavenumbers = np.flip(np.linspace(Min_Wavenum, Max_Wavenum, Spectrum_Pts))
+
+        f.seek(288);
+
+        Flag=0
+        while Flag != 3:
+            Flag = np.fromfile(f, np.uint16, 1)
+
+        DataPosition=np.fromfile(f,np.uint16, 1)
+        f.seek(DataPosition[0])
+
+        Spectra = np.fromfile(f, np.single, Spectrum_Pts[0])
+    return Spectra, Wavenumbers, SpectraTitles
