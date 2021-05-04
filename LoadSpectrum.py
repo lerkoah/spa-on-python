1dedfe9a5521846ba083de730c91a8858425899e
lerkoah <lerko.araya@ug.uchile.cl>
2021-05-04 11:48:40 -0400

    array updates

--- LoadSpectrum.py
+++ LoadSpectrum.py
@@ -10,14 +10,15 @@ def read_spa(filepath):
     '''
     with open(filepath, 'rb') as f:
         f.seek(564)
-        Spectrum_Pts = np.fromfile(f, np.int32,1)
+        Spectrum_Pts = np.fromfile(f, np.int32,1)[0]
         f.seek(30)
         SpectraTitles = np.fromfile(f, np.uint8,255)
         SpectraTitles = ''.join([chr(x) for x in SpectraTitles if x!=0])
 
         f.seek(576)
-        Max_Wavenum=np.fromfile(f, np.single, 1)
-        Min_Wavenum=np.fromfile(f, np.single, 1)
+        Max_Wavenum=np.fromfile(f, np.single, 1)[0]
+        Min_Wavenum=np.fromfile(f, np.single, 1)[0]
+        print(Min_Wavenum, Max_Wavenum, Spectrum_Pts)
         Wavenumbers = np.flip(np.linspace(Min_Wavenum, Max_Wavenum, Spectrum_Pts))
 
         f.seek(288);
@@ -29,5 +30,5 @@ def read_spa(filepath):
         DataPosition=np.fromfile(f,np.uint16, 1)
         f.seek(DataPosition[0])
 
-        Spectra = np.fromfile(f, np.single, Spectrum_Pts[0])
+        Spectra = np.fromfile(f, np.single, Spectrum_Pts)
     return Spectra, 1e7/Wavenumbers, SpectraTitles
